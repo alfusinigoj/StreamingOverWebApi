045c78461b85150c6f6ad908c3f59049a6d3fbc7
Alfus Inigo Jaganathan <ajaganathan@vmware.com>
2021-07-29 00:34:13 -0400

    cancellation token used on Ctrl+C

--- Client/Program.cs
+++ Client/Program.cs
@@ -1,7 +1,4 @@
 ﻿using System;
-using System.CommandLine;
-using System.CommandLine.Invocation;
-using System.CommandLine.IO;
 using System.IO;
 using System.Net.Http;
 using System.Threading;
@@ -11,29 +8,14 @@ namespace Client
 {
     class Program
     {
-        static async Task<int> Main(string[] args)
+        static async Task Main(string[] args)
         {
-            var command = new RootCommand();
-            command.Handler = CommandHandler.Create(async (IConsole console, CancellationToken token) =>
-            {
-                try
-                {
-                    await ReadFromStream(console, token);
-                    return 0;
-                }
-                catch (OperationCanceledException)
-                {
-                    console.Error.WriteLine("The operation was aborted");
-                    return 1;
-                }
-            });
-
-            return await command.InvokeAsync(args);
+            await ReadFromStreamAsync(GetCancellationToken());
         }
 
-        private static async Task ReadFromStream(IConsole console, CancellationToken token)
+        static async Task ReadFromStreamAsync(CancellationToken token)
         {
-            var httpClient = new HttpClient();
+            using var httpClient = new HttpClient();
             var stream = await httpClient.GetStreamAsync("https://localhost:7001/streams/300", token);
             try
             {
@@ -42,14 +24,25 @@ namespace Client
                     while (!reader.EndOfStream)
                     {
                         var line = await reader.ReadLineAsync();
-                        console.Out.WriteLine(line);
+                        Console.Out.WriteLine(line);
                     }
                 }
             }
             catch (Exception ex)
             {
-                console.Error.WriteLine(ex.ToString());
+                Console.Error.WriteLine(ex.ToString());
             }
+        }
+
+        static CancellationToken GetCancellationToken()
+        {
+            var tokenSource = new CancellationTokenSource();
+            Console.CancelKeyPress += (sender, e) => 
+            {
+                e.Cancel = false;
+                tokenSource.Cancel();
+            };
+            return tokenSource.Token;
         }
     }
 }
